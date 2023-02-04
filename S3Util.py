388f72b1e55da2d949d13ae5ee69483fedda1308
manideepakreddyaila <aila.m@northeastern.edu>
2023-02-03 20:51:49 -0500

    refactored the code

---
+++ S3Util.py
@@ -0,0 +1,13 @@
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+
+
+class S3Util:
+    def __init__(self, resource, bucket_name):
+        self.s3 = boto3.client(resource, config=Config(signature_version=UNSIGNED))
+        self.bucket_name = bucket_name
+
+    def get_pages(self, prefix=''):
+        paginator = self.s3.get_paginator('list_objects')
+        return paginator.paginate(Bucket=self.bucket_name, Prefix=prefix)
--- dbUtil.py
+++ dbUtil.py
@@ -1,11 +1,9 @@
-import boto3
+from S3Util import S3Util
 import sqlite3
-from botocore import UNSIGNED
-from botocore.config import Config
-
 
 class DbUtil:
     conn = None
+
     def __init__(self, db_file_name):
         self.conn = sqlite3.connect(db_file_name)
         self.cursor = self.conn.cursor()
@@ -18,11 +16,12 @@ class DbUtil:
         self.conn.commit()
 
     def insert(self, table_name, column_names, list_of_tuples):
-        print('INSERT INTO {} ({}) VALUES ({})'.format(table_name, ', '.join([i for i in column_names]), ', '.join(['?' for i in range(len(column_names))])))
-        util.conn.executemany('INSERT INTO {} ({}) VALUES ({})'.format(table_name, ', '.join([i for i in column_names]), ', '.join(['?' for i in range(len(column_names))])), list_of_tuples)
+        util.conn.executemany('INSERT INTO {} ({}) VALUES ({})'
+                              .format(table_name,
+                                      ', '.join([i for i in column_names]),
+                                      ', '.join(['?' for _ in range(len(column_names))])),
+                              list_of_tuples)
         util.conn.commit()
-        print('committed')
-
 
     def filter(self, table_name, req_value, **input_values):
         query = '''SELECT DISTINCT {} from {}  WHERE'''.format(req_value, table_name)
@@ -31,43 +30,84 @@ class DbUtil:
         if query.endswith('AND'):
             query = query[:-4]
         self.cursor.execute(query, tuple(input_values.values()))
-        return self.cursor.fetchall()
+        l = self.cursor.fetchall()
+        return sorted([x[0] for x in l])
+
 
 if __name__ == '__main__':
-    s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-    paginator = s3.get_paginator('list_objects')
-    pages = paginator.paginate(Bucket='noaa-goes18', Prefix='ABI-L1b-RadC')
-    util = DbUtil('metadata1.db')
+    column_names_geos = {'id': 'INTEGER PRIMARY KEY AUTOINCREMENT', 'product': 'TEXT', 'year': 'TEXT', 'day_of_year': 'TEXT', 'hour': 'TEXT'}
+    column_names_nexrad = {'id': 'INTEGER PRIMARY KEY AUTOINCREMENT', 'year': 'TEXT', 'month': 'TEXT', 'day': 'TEXT', 'station': 'TEXT'}
+    db_file_name = 'metadata.db'
+    bucket_name_geos = 'noaa-goes18'
+    bucket_name_nexrad = 'noaa-nexrad-level2'
+    table_name_geos = 'geos18'
+    table_name_nexrad = 'nexrad'
+    resource_name = 's3'
+
+    # s3util = S3Util(resource_name, bucket_name_nexrad)
+    s3util = S3Util(resource_name, bucket_name_geos)
+    pages = s3util.get_pages(prefix='ABI-L1b-RadC')
+    # pages = s3util.get_pages(prefix='2023')
+    util = DbUtil(db_file_name=db_file_name)
     try:
+        # GEOS
         # creation of table
-        column_names = ['id INTEGER PRIMARY KEY', 'product TEXT', 'year TEXT', 'day_of_year TEXT', 'hour TEXT']
-        util.create_table('geos18', *column_names)
+        columns_to_create = [key + ' ' + value for key, value in column_names_geos.items()]
+        column_keys = list(column_names_geos.keys())
+        util.create_table(table_name_geos, *columns_to_create)
 
         # insertion of rows
         rows = set()
         for page in pages:
-            for i, obj in enumerate(page['Contents']):
-                print('i', i)
+            for obj in page['Contents']:
                 levels = obj['Key'].split('/')
-                if(len(levels) == 5):
+                if len(levels) == 5:
                     product = levels[0]
                     year = levels[1]
                     day_of_year = levels[2]
                     hour = levels[3]
                     rows.add((product, year, day_of_year, hour))
-                if ((i+1) % 1000 == 0):
-                    print('hi')
-                    util.insert('geos18', ['product', 'year', 'day_of_year', 'hour'],rows)
+                if len(rows) % 1000 == 0:
+                    util.insert(table_name_geos, column_keys[1:], rows)
                     rows = set()
         if rows:
-            util.insert('geos18', ['product', 'year', 'day_of_year', 'hour'],rows)
+            util.insert(table_name_geos, column_keys[1:], rows)
 
         cursor = util.conn.cursor()
-        cursor.execute("SELECT count(*) FROM geos18")
-        result = cursor.fetchall()
-        print(result)
-        # print(util.filter('geos18', 'day_of_year', product = 'ABI-L1b-RadC', year = '2022'))
-        # print(util.filter('geos18', 'year', product = 'ABI-L1b-RadC'))
-        print(util.filter('geos18', 'hour', product = 'ABI-L1b-RadC', year = '2022', day_of_year = '209'))
+
+        # filtering of rows
+        print(util.filter(table_name_geos, '', product='ABI-L1b-RadC', year='2022', day_of_year='209'))
+        print(util.filter(table_name_geos, 'day_of_year', product='ABI-L1b-RadC', year='2022'))
+        print(util.filter(table_name_geos, 'year', product='ABI-L1b-RadC'))
+
+        # NEXRAD
+        # creation of table
+        # columns_to_create = [key + ' ' + value for key, value in column_names_nexrad.items()]
+        # column_keys = list(column_names_nexrad.keys())
+        # util.create_table(table_name_nexrad, *columns_to_create)
+
+        # insertion of rows
+        # rows = set()
+        # for page in pages:
+        #     for obj in page['Contents']:
+        #         levels = obj['Key'].split('/')
+        #         if len(levels) == 5:
+        #             year = levels[0]
+        #             month = levels[1]
+        #             day = levels[2]
+        #             station = levels[3]
+        #             rows.add((year, month, day, station))
+        #         if len(rows) % 1000 == 0:
+        #             util.insert(table_name_nexrad, column_keys[1:], rows)
+        #             rows = set()
+        # if rows:
+        #     util.insert(table_name_nexrad, column_keys[1:], rows)
+        #
+        # cursor = util.conn.cursor()
+
+        # filtering of rows
+        # print(util.filter(table_name_nexrad, 'month', year='2022'))
+        # print(util.filter(table_name_nexrad, 'day', year='2022', month='11'))
+        # print(util.filter(table_name_nexrad, 'station', year='2022', month='11', day='30'))
     finally:
         util.conn.close()
