adb424bca6bbc28533a023d1e490e615efcd8fef
Raj Mehta <>
2023-02-09 18:26:44 -0500

    Added functionality

--- main.py
+++ main.py
@@ -9,6 +9,71 @@ from botocore import UNSIGNED
 from botocore.config import Config
 import string
 import webbrowser
+<<<<<<< Updated upstream
+=======
+from decouple import config
+# Import class from dbUtil
+import dbUtil
+# Import AWS Logging
+from aws_logging import write_logs
+from dbUtil import *
+
+util = DbUtil("metadata.db")
+########################################################################################################################
+# AWS Destination Credentials:
+aws_access_key_id = config('aws_access_key_id')
+aws_secret_access_key = config('aws_secret_access_key')
+# AWS CloudWatch Credentials:
+# log_aws_access_key_id = config('log_aws_access_key_id')
+# log_aws_secret_access_key = config('log_aws_secret_access_key')
+# Destination S3 Directory:
+dest_bucket = 'damg7245'
+dest_folder = 'assignment1'
+########################################################################################################################
+
+st.title('SEVIR Data Fetcher')
+
+# CREATE TABLE:
+# util = DbUtil('metadata1.db')
+# column_names = ['id INTEGER PRIMARY KEY', 'product TEXT', 'year TEXT', 'day_of_year TEXT', 'hour TEXT']
+# util.create_table('geos18', *column_names)
+
+# SELECT DATASOURCE:
+data_source = st.selectbox('Data Source: ', ['GOES-18 geostationary satellite', 'NEXRAD weather radars'])
+
+# Search Method:
+search_method = st.selectbox('Search Method: ', ['File Name', 'Field Selection'])
+if data_source == 'GOES-18 geostationary satellite':
+    BUCKET_NAME = 'noaa-goes18'
+    core_url = 'https://noaa-goes18.s3.amazonaws.com/{product_fn}/{year}/{day_of_year}/{hour}/{file_name}'
+    metadata = ['Product', 'Year', 'Day of Year', 'Hour', 'File Name']
+    if search_method == 'Field Selection':
+        st.markdown(f"""
+    URL Format: {core_url}\n
+    Required Fields:
+    - Product
+    - Year
+    - Day of Year
+    - Hour
+    - File Name
+    """)
+elif data_source == 'NEXRAD weather radars':
+    BUCKET_NAME = 'noaa-nexrad-level2'
+    core_url = 'https://noaa-nexrad-level2.s3.amazonaws.com/{year}/{month}/{day}/{nexrad_station}/{file_name}'
+    metadata = ['Year', 'Month', 'Day', 'NEXRAD Station', 'File Name']
+    if search_method == 'Field Selection':
+        st.markdown(f"""
+URL Format: {core_url}\n
+Required Fields:
+- Year
+- Month
+- Day
+- NEXRAD Station
+- File Name
+""")
+
+
+>>>>>>> Stashed changes
 
 first_level = []
 year = []
@@ -66,6 +131,7 @@ if prod_selected:                                                               
     year_selected = st.selectbox(
     'Please select year', year)
 
+<<<<<<< Updated upstream
 
     if year_selected:      
                                                                                      #year selected
@@ -96,11 +162,45 @@ if prod_selected:                                                               
             if days_selected:      
                                                                                      #months selected
                 prefix = prod_selected+year_selected+months_selected+days_selected
+=======
+if search_method == 'Field Selection' and data_source == 'GOES-18 geostationary satellite':
+    result = s3.list_objects_v2(Bucket=BUCKET_NAME, Delimiter="/")
+    folders = [fld["Prefix"] for fld in result["CommonPrefixes"]]
+
+    for folder in folders:
+        first_level.append(folder)
+
+    st.write("Selected Product is ABI-L1b-RadC")
+
+    year_list =util.filter("geos18", 'year', product='ABI-L1b-RadC')
+    year_list.insert(0, "")
+    year_selected = st.selectbox(
+    f'Please select {metadata[1]}', year_list)
+
+    if year_selected:      
+
+        day_list =util.filter("geos18", 'day_of_year', product='ABI-L1b-RadC', year=year_selected)
+        day_list.insert(0, "")
+        day_selected = st.selectbox(
+        f'Please select {metadata[2]}', day_list)
+
+        if day_selected:      
+                                                                                   #months selected
+            hour_list =util.filter("geos18", 'hour', product='ABI-L1b-RadC', year=year_selected, day_of_year=day_selected)
+            hour_list.insert(0, "")
+            hour_selected = st.selectbox(
+            f'Please select {metadata[3]}', hour_list)
+
+            if hour_selected:      
+                                                                                    #months selected
+                prefix = "ABI-L1b-RadC/"+year_selected+"/"+day_selected+"/"+hour_selected+"/"
+>>>>>>> Stashed changes
                 result = s3.list_objects(Bucket=BUCKET_NAME, Prefix=prefix, Delimiter='/')
                 for item in result['Contents']:
                     file = item['Key']
                     files.append(file)
 
+<<<<<<< Updated upstream
                 for i in range(len(files)):
                     files[i] = files[i].replace(prefix, '')
 
@@ -111,14 +211,242 @@ if prod_selected:                                                               
                     link = find_url_from_filename(files_selected)
                     st.write(link)
 
+=======
+                st.write(f'Total Files Available: {len(files)}')
+
+                for i in range(len(files)):
+                    files[i] = files[i].replace(prefix, '')
+
+                # Download all files in folder
+                files.insert(1, 'Download All Files')
+
+
+                files_selected = st.selectbox(
+                f'Please select {metadata[4]}', files)
+
+                if files_selected == 'Download All Files':
+                    s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+                    # Total Folder Size
+                    s3_folder = prefix
+                    bytes = sum([object.size for object in s3.Bucket(BUCKET_NAME).objects.filter(Prefix=s3_folder)])
+                    st.write(f'Total Folder Size: {round(bytes//1000/1024/1024, 3)} GB')
+
+                    if st.button('Download All Files'):
+                        # TODO
+                        download_s3_folder(BUCKET_NAME, s3_folder, local_dir=None)
+
+                elif files_selected:
+                    url = filename_url_producer(BUCKET_NAME, files_selected)
+                    st.write(url)
+                    user_inputs = [year_selected, day_selected, hour_selected, files_selected]
+                    write_logs(f'User Input: {user_inputs}')
+                    write_logs(f'Generated URL: {url}')
+
+                    # TESTING
+                    st.write('TEST:')
+                    st.write(f'Prefix = {prefix}  \n Files_selected = {files_selected}')
+>>>>>>> Stashed changes
                     st.text("")
                     st.text("")
                     st.text("")
 
+<<<<<<< Updated upstream
                     if st.button('Download File'):
                         webbrowser.open_new_tab(link)
+=======
 
+                    if (search_method == 'File Name' and file_name_input) or (search_method == 'Field Selection' and files_selected):
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            if st.button('Download File'):
+                                write_logs('User Action: Downloaded File Locally')
+                                webbrowser.open_new_tab(url)
+                                st.write('File Downloaded Locally')
 
+                        with col2:
+                            if st.button('Transfer File to S3 Bucket'):
+                                dest_url = copy_file_to_dest_s3(BUCKET_NAME, dest_bucket, dest_folder, prefix, files_selected)
+                                write_logs(f'User Action: Transfered file to S3 Bucket - {dest_url}')
+                                if 'Error' in dest_url:
+                                    st.write(dest_url)
+                                st.write(f'Destination s3 URL: {dest_url}')
+
+elif search_method == 'Field Selection' and data_source == 'NEXRAD weather radars':
+    BUCKET_NAME = 'noaa-nexrad-level2'
+    result = s3.list_objects_v2(Bucket=BUCKET_NAME, Delimiter="/")
+    folders = [fld["Prefix"] for fld in result["CommonPrefixes"]]
+>>>>>>> Stashed changes
+
+    for folder in folders:
+        first_level.append(folder)
+
+    st.write("Selected Year is 2023")
+
+    month_list =util.filter("nexrad", 'month', year='2023')
+    month_list.insert(0, "")
+    month_selected = st.selectbox(
+    f'Please select {metadata[1]}', month_list)
+
+    if month_selected:      
+
+        day_list =util.filter("nexrad", 'day', year='2023', month=month_selected)
+        day_list.insert(0, "")
+        day_selected = st.selectbox(
+        f'Please select {metadata[2]}', day_list)
+
+        if day_selected:      
+                                                                                   #months selected
+            station_list =util.filter("nexrad", 'station', year='2023', month=month_selected, day=day_selected)
+            station_list.insert(0, "")
+            station_selected = st.selectbox(
+            f'Please select {metadata[3]}', station_list)
+
+            if station_selected:      
+                                                                                #months selected
+                prefix = "2023/"+month_selected+"/"+day_selected+"/"+station_selected+"/"
+                result = s3.list_objects(Bucket=BUCKET_NAME, Prefix=prefix, Delimiter='/')
+                for item in result['Contents']:
+                    file = item['Key']
+                    files.append(file)
+
+                st.write(f'Total Files Available: {len(files)}')
+
+                for i in range(len(files)):
+                    files[i] = files[i].replace(prefix, '')
+
+                # Download all files in folder
+                files.insert(1, 'Download All Files')
+
+                files_selected = st.selectbox(
+                f'Please select {metadata[4]}', files)
+
+                if files_selected == 'Download All Files':
+                    s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+                    # Total Folder Size
+                    s3_folder = prefix
+                    bytes = sum([object.size for object in s3.Bucket(BUCKET_NAME).objects.filter(Prefix=s3_folder)])
+                    st.write(f'Total Folder Size: {round(bytes//1000/1024/1024, 3)} GB')
+
+                if st.button('Download All Files'):
+                    # TODO
+                    download_s3_folder(BUCKET_NAME, s3_folder, local_dir=None)
+                elif files_selected:
+                    st.write(files_selected)
+                    url = filename_url_producer(BUCKET_NAME, files_selected)
+                    st.write(url)
+                    user_inputs = ["2023", month_selected, day_selected, station_selected, files_selected]
+                    write_logs(f'User Input: {user_inputs}')
+                    write_logs(f'Generated URL: {url}')
+
+                    # TESTING
+                    st.write('TEST:')
+                    st.write(f'Prefix = {prefix}  \n Files_selected = {files_selected}')
+                    st.text("")
+                    st.text("")
+                    st.text("")
+
+                    if (search_method == 'File Name' and file_name_input) or (search_method == 'Field Selection' and files_selected):
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            if st.button('Download File'):
+                                write_logs('User Action: Downloaded File Locally')
+                                webbrowser.open_new_tab(url)
+                                st.write('File Downloaded Locally')
+
+                        with col2:
+                            if st.button('Transfer File to S3 Bucket'):
+                                dest_url = copy_file_to_dest_s3(BUCKET_NAME, dest_bucket, dest_folder, prefix, files_selected)
+                                write_logs(f'User Action: Transfered file to S3 Bucket - {dest_url}')
+                                if 'Error' in dest_url:
+                                    st.write(dest_url)
+                                st.write(f'Destination s3 URL: {dest_url}')
         
 
 
+<<<<<<< Updated upstream
+=======
+# {
+#       "expectation_type": "expect_table_row_count_to_be_between",
+#       "kwargs": {
+#         "max_value": 100000,
+#         "min_value": 0
+#       },
+#       "meta": {
+#         "profiler_details": {
+#           "metric_configuration": {
+#             "domain_kwargs": {},
+#             "metric_name": "table.row_count",
+#             "metric_value_kwargs": null
+#           },
+#           "num_batches": 1
+#         }
+#       }
+#     },
+#     {
+#       "expectation_type": "expect_compound_columns_to_be_unique",
+#       "kwargs": {
+#         "column_list": [
+#           "product",
+#           "year",
+#           "day_of_year",
+#           "hour"
+#         ]
+#       },
+#       "meta": {}
+#     },
+#     {
+#       "expectation_type": "expect_column_values_to_not_be_null",
+#       "kwargs": {
+#         "column": "product"
+#       },
+#       "meta": {}
+#     },
+#     {
+#       "expectation_type": "expect_column_values_to_not_be_null",
+#       "kwargs": {
+#         "column": "year"
+#       },
+#       "meta": {}
+#     },
+#     {
+#       "expectation_type": "expect_column_values_to_not_be_null",
+#       "kwargs": {
+#         "column": "day_of_year"
+#       },
+#       "meta": {}
+#     },
+#     {
+#       "expectation_type": "expect_column_values_to_not_be_null",
+#       "kwargs": {
+#         "column": "hour"
+#       },
+#       "meta": {}
+#     },
+#     {
+#       "expectation_type": "expect_column_values_to_be_in_set",
+#       "kwargs": {
+#         "column": "product",
+#         "value_set": [
+#           "ABI-L1b-RadC"
+#         ]
+#       },
+#       "meta": {}
+#     },
+#     {
+#       "expectation_type": "expect_table_columns_to_match_set",
+#       "kwargs": {
+#         "column_set": [
+#           "day_of_year",
+#           "product",
+#           "year",
+#           "hour",
+#           "id"
+#         ]
+#       },
+#       "meta": {
+#         "profiler_details": {
+#           "success_ratio": 1.0
+#         }
+#       }
+#     }
+>>>>>>> Stashed changes
